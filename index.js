28c4eebf9e165c1b0b1f28065f70529f05724a2d
Shawn Flock <sflock@navicamls.net>
2018-10-09 09:51:57 -0400

    commiting shit

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const express = require("express");
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
+
+
+mongoose.connect(mongoURI);
 const app = express();
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-  console.log(accessToken);
-}));
+require('./routes/authRoutes')(app);
 
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.get("/", (req, res) => {
   res.send({
@@ -25,4 +16,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT); 
+app.listen(PORT);
