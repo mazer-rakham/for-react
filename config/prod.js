5cbb5700eca7f653bfb704b444741851df6e240d
Shawn Flock <sflock@navicamls.net>
2018-10-23 13:51:14 -0400

    stufff

--- config/prod.js
+++ config/prod.js
@@ -4,29 +4,3 @@ module.exports = {
     mongoURI:process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 }
-
-
-
-
-
-
-
-
-
-// mongodb://GradumAnte:Ayumi9822@ds239703.mlab.com:39703/emaily-prod-1
-
-
-// {
-//     "web": {
-//       "client_id": "362360105228-ojft2mjcm55v93mufkkom21asl4mcbdf.apps.googleusercontent.com",
-//       "project_id": "emaily-prod-220313",
-//       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-//       "token_uri": "https://www.googleapis.com/oauth2/v3/token",
-//       "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-//       "client_secret": "rUkX598Gz6n6d-YRKOTYujnN",
-//       "redirect_uris": [
-//         "https://infinite-bayou-88605.herokuapp.com/auth/google/callback"
-//       ],
-//       "javascript_origins": ["https://infinite-bayou-88605.herokuapp.com"]
-//     }
-//   }
